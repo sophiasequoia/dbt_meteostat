93789d32b3a25c9cf5698de07749f640dedd110a
Sophia Schroth <sophiaschroth_nf@Sophias-MacBook-Air.local>
2026-04-20 11:23:08 +0200

    northwind project staging_sources.yml, then STEP 1: staging models of categories, order_details, orders, products

---
+++ models/northwind/models/staging/staging_products.sql
@@ -0,0 +1,46 @@
+/*- Use `{{ source() }}` to pull data from the raw tables.
+
+- Rename columns to snake_case.
+
+- Cast dates and numbers to the correct types.
+
+- Keep only relevant columns.*/
+
+
+WITH source_data AS ( -- this source_data cte thing is apparently just dbt convention, according to Hilal
+    SELECT *
+    FROM {{ source('northwind', 'products') }}
+)
+SELECT productid AS product_id
+	   , productname AS product_name
+	   , supplierid AS supplier_id
+	   , categoryid AS category_id
+	   , quantityperunit AS quantity_per_unit
+	   , unitprice::NUMERIC AS unit_price
+	   , unitsinstock::INT AS units_in_stock
+	   , unitsonorder::INT AS units_on_order
+	   , reorderlevel AS reorder_level
+	   , discontinued
+FROM source_data
+
+
+
+-- as query for testing:
+/*
+
+SELECT productid AS product_id
+	   , productname AS product_name
+	   , supplierid AS supplier_id
+	   , categoryid AS category_id
+	   , quantityperunit AS quantity_per_unit
+	   , unitprice::NUMERIC AS unit_price
+	   , unitsinstock::INT AS units_in_stock
+	   , unitsonorder::INT AS units_on_order
+	   , reorderlevel AS reorder_level
+	   , discontinued
+FROM northwind.products
+;
+*/
+
+
+
