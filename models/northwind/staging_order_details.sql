fd7316801365d983ce205e5aa26c0a039d8c720c
Sophia Schroth <sophiaschroth_nf@Sophias-MacBook-Air.local>
2026-04-20 13:02:57 +0200

    finally correct four staging files without the parsing issue

--- models/northwind/staging_order_details.sql
+++ models/northwind/staging_order_details.sql
@@ -1,4 +1,13 @@
-WITH source_data AS (
+/*- Use jinja source function to pull data from the raw tables.
+
+- Rename columns to snake_case.
+
+- Cast dates and numbers to the correct types.
+
+- Keep only relevant columns.*/
+
+
+WITH source_data AS ( -- this source_data cte thing is apparently just dbt convention, according to Hilal
     SELECT *
     FROM {{source('northwind_data', 'order_details')}}
 )
@@ -8,3 +17,19 @@ SELECT orderid AS order_id
 	   , quantity::INT AS quantity
 	   , discount::NUMERIC
 FROM source_data
+
+
+-- as query for testing:
+/*
+
+SELECT * FROM northwind.order_details;
+
+SELECT orderid AS order_id
+	   , productid AS product_id
+	   , unitprice::NUMERIC AS unit_price
+	   , quantity::INT AS quantity
+	   , discount::NUMERIC
+FROM northwind.order_details
+;
+*/
+
---
+++ models/northwind/staging_orders.sql
@@ -0,0 +1,54 @@
+/*- Use jinja source function to pull data from the raw tables.
+
+- Rename columns to snake_case.
+
+- Cast dates and numbers to the correct types.
+
+- Keep only relevant columns.*/
+
+
+WITH source_data AS ( -- this source_data cte thing is apparently just dbt convention, according to Hilal
+    SELECT *
+    FROM {{source('northwind_data', 'orders')}}
+)
+SELECT orderid AS order_id
+	   , customerid AS customer_id
+	   , employeeid AS employee_id
+	   , orderdate::DATE AS order_date
+	   , requireddate::DATE AS required_date
+	   , shippeddate::DATE AS shipped_date
+	   , shipvia AS ship_via
+	   , freight
+	   , shipname AS ship_name
+	   , shipaddress AS ship_address
+	   , shipcity AS ship_city
+	   , shipregion AS ship_region
+	   , shippostalcode AS ship_postal_code
+	   , shipcountry AS ship_country
+FROM source_data
+
+
+-- as query for testing:
+
+/*
+
+SELECT orderid AS order_id
+	   , customerid AS customer_id
+	   , employeeid AS employee_id
+	   , orderdate::DATE AS order_date
+	   , requireddate::DATE AS required_date
+	   , shippeddate::DATE AS shipped_date
+	   , shipvia AS ship_via
+	   , freight
+	   , shipname AS ship_name
+	   , shipaddress AS ship_address
+	   , shipcity AS ship_city
+	   , shipregion AS ship_region
+	   , shippostalcode AS ship_postal_code
+	   , shipcountry AS ship_country
+FROM northwind.orders
+;*/
+
+
+
+
